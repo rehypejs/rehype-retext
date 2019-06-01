d6dc0e9ce89bde3042705f1de56379107a9a0b5d
Titus Wormer <tituswormer@gmail.com>
2019-06-01 11:10:55 +0200

    Refactor comment-style

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ var hast2nlcst = require('hast-util-to-nlcst')
 
 module.exports = rehype2retext
 
-// Attacher.  If a destination processor is given, runs the destination with
-// the new nlcst tree (bridge-mode).  If a parser is given, returns the nlcst
-// tree: further plugins run on that tree (mutate-mode).
+// Attacher.
+// If a destination processor is given, runs the destination with the new nlcst
+// tree (bridge-mode).
+// If a parser is given, returns the nlcst tree: further plugins run on that
+// tree (mutate-mode).
 function rehype2retext(destination) {
   var fn = destination && destination.run ? bridge : mutate
   return fn(destination)
 }
 
-// Mutate-mode.  Further transformers run on the nlcst tree.
+// Mutate-mode.
+// Further transformers run on the nlcst tree.
 function mutate(parser) {
   return transformer
   function transformer(node, file) {
@@ -20,7 +23,8 @@ function mutate(parser) {
   }
 }
 
-// Bridge-mode.  Runs the destination with the new nlcst tree.
+// Bridge-mode.
+// Runs the destination with the new nlcst tree.
 function bridge(destination) {
   return transformer
   function transformer(node, file, next) {
