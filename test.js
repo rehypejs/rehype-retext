7ccbac192d88b77288b03fd73d8b00f1075e940a
Titus Wormer <tituswormer@gmail.com>
2023-08-31 14:09:31 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {unified} from 'unified'
 import rehypeParse from 'rehype-parse'
 import {ParseEnglish} from 'parse-english'
@@ -20,49 +21,45 @@ const doc = [
   '</html>'
 ].join('\n')
 
-test('rehypeRetext', async (t) => {
-  t.throws(
-    () => {
-      // @ts-expect-error: runtime.
+test('rehypeRetext', async function (t) {
+  await t.test('should throw when w/o parser or processor', async function () {
+    assert.throws(function () {
+      // @ts-expect-error: check how missing options is handled.
       unified().use(rehypeRetext).freeze()
-    },
-    /Expected `parser` \(such as from `parse-english`\) or `processor` \(a unified pipeline\) as `options`/,
-    'should throw when w/o parser or processor'
-  )
+    }, /Expected `parser` \(such as from `parse-english`\) or `processor` \(a unified pipeline\) as `options`/)
+  })
 
-  t.equal(
-    unified()
+  await t.test('should mutate', async function () {
+    const file = await unified()
       .use(rehypeParse, {fragment: true})
       .use(rehypeRetext, ParseEnglish)
       // @ts-expect-error: to do: remove this when `retext-stringify` is released.
       .use(retextStringify)
-      .processSync(doc)
-      .toString(),
-    'Bravo',
-    'should mutate'
-  )
+      .process(doc)
 
-  const file = await unified()
-    .use(rehypeParse)
-    // @ts-expect-error: to do: remove this when `retext-stringify` is released, hopefully.
-    .use(rehypeRetext, unified().use(retextEnglish))
-    .use(rehypeStringify)
-    .process(doc)
+    assert.equal(String(file), 'Bravo')
+  })
 
-  t.equal(
-    file.toString(),
-    [
-      '<!doctype html><html><head>',
-      '    <meta charset="utf8">',
-      '    <title>Alpha</title>',
-      '  </head>',
-      '  <body>',
-      '    <p>Bravo</p>',
-      '  ',
-      '</body></html>'
-    ].join('\n'),
-    'should bridge'
-  )
+  await t.test('should bridge', async function () {
+    const file = await unified()
+      .use(rehypeParse)
+      // @ts-expect-error: to do: remove this when `retext-stringify` is released, hopefully.
+      .use(rehypeRetext, unified().use(retextEnglish))
+      .use(rehypeStringify)
+      .process(doc)
 
-  t.end()
+    assert.equal(
+      String(file),
+      [
+        '<!doctype html><html><head>',
+        '    <meta charset="utf8">',
+        '    <title>Alpha</title>',
+        '  </head>',
+        '  <body>',
+        '    <p>Bravo</p>',
+        '  ',
+        '</body></html>'
+      ].join('\n')
+    )
+  })
 })
