b5c4165a7d50afee9f7355e4cfa9cb96158d0b02
Titus Wormer <tituswormer@gmail.com>
2023-08-31 14:04:04 +0200

    Update `@types/hast`, `unified`, utilities, etc

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-/**
- * @typedef {import('./lib/index.js').Parser} Parser
- * @typedef {import('./lib/index.js').Processor} Processor
- */
-
 export {default} from './lib/index.js'
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 import test from 'tape'
 import {unified} from 'unified'
 import rehypeParse from 'rehype-parse'
-import retextEnglish, {Parser as RetextEnglish} from 'retext-english'
+import {ParseEnglish} from 'parse-english'
+import retextEnglish from 'retext-english'
 import rehypeStringify from 'rehype-stringify'
 import retextStringify from 'retext-stringify'
 import rehypeRetext from './index.js'
@@ -19,20 +20,21 @@ const doc = [
   '</html>'
 ].join('\n')
 
-test('rehypeRetext', (t) => {
+test('rehypeRetext', async (t) => {
   t.throws(
     () => {
       // @ts-expect-error: runtime.
       unified().use(rehypeRetext).freeze()
     },
-    /Expected `parser` \(such as from `retext-english` or `parse-english`\) or `processor` \(a unified pipeline\) as `options`/,
+    /Expected `parser` \(such as from `parse-english`\) or `processor` \(a unified pipeline\) as `options`/,
     'should throw when w/o parser or processor'
   )
 
   t.equal(
     unified()
       .use(rehypeParse, {fragment: true})
-      .use(rehypeRetext, RetextEnglish)
+      .use(rehypeRetext, ParseEnglish)
+      // @ts-expect-error: to do: remove this when `retext-stringify` is released.
       .use(retextStringify)
       .processSync(doc)
       .toString(),
@@ -40,13 +42,15 @@ test('rehypeRetext', (t) => {
     'should mutate'
   )
 
+  const file = await unified()
+    .use(rehypeParse)
+    // @ts-expect-error: to do: remove this when `retext-stringify` is released, hopefully.
+    .use(rehypeRetext, unified().use(retextEnglish))
+    .use(rehypeStringify)
+    .process(doc)
+
   t.equal(
-    unified()
-      .use(rehypeParse)
-      .use(rehypeRetext, unified().use(retextEnglish))
-      .use(rehypeStringify)
-      .processSync(doc)
-      .toString(),
+    file.toString(),
     [
       '<!doctype html><html><head>',
       '    <meta charset="utf8">',
